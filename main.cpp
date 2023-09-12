7ba5b7721eee8a01f9bd6dd2191b18f06a3664d5
Rachel Jones <racheljones3@comcast.net>
2023-09-12 13:47:04 -0700

    additional edits

--- main.cpp
+++ main.cpp
@@ -6,6 +6,106 @@
 
 // define default capacity of the queue
 
+//// Class for queue
+//class queue
+//{
+//    int* arr;		// array to store queue elements
+//    int capacity;	// maximum capacity of the queue
+//    int front;		// front points to front element in the queue (if any)
+//    int rear;		// rear points to last element in the queue
+//    int count;		// current size of the queue
+//
+//public:
+//    queue(int size = SIZE); 	// constructor
+//    ~queue();   				// destructor
+//
+//    void dequeue();
+//    void enqueue(int x);
+//    int peek();
+//    int size();
+//    bool isEmpty();
+//    bool isFull();
+//};
+//
+//// Constructor to initialize queue
+//queue::queue(int size)
+//{
+//    arr = new int[size];
+//    capacity = size;
+//    front = 0;
+//    rear = -1;
+//    count = 0;
+//}
+//
+//// Destructor to free memory allocated to the queue
+//queue::~queue()
+//{
+//    delete arr; // you are not required to test this function;
+//    // however, are there issues with it?
+//}
+//
+//// Utility function to remove front element from the queue
+//void queue::dequeue()
+//{
+//    // check for queue underflow
+//    if (isEmpty())
+//    {
+//        cout << "UnderFlow\nProgram Terminated\n";
+//        return;
+//    }
+//
+//    cout << "Removing " << arr[front] << '\n';
+//
+//    front = (front + 1) % capacity;
+//    count--;
+//}
+//
+//// Utility function to add an item to the queue
+//void queue::enqueue(int item)
+//{
+//    // check for queue overflow
+//    if (isFull())
+//    {
+//        cout << "OverFlow\nProgram Terminated\n";
+//        return;
+//    }
+//
+//    cout << "Inserting " << item << '\n';
+//
+//    rear = (rear + 1) % capacity;
+//    arr[rear] = size();
+//    count++;
+//}
+//
+//// Utility function to return front element in the queue
+//int queue::peek()
+//{
+//    if (isEmpty())
+//    {
+//        cout << "UnderFlow\nProgram Terminated\n";
+//        return numeric_limits<int>::min();
+//    }
+//    return arr[rear];
+//}
+//
+//// Utility function to return the size of the queue
+//int queue::size()
+//{
+//    return count + 1;
+//}
+//
+//// Utility function to check if the queue is empty or not
+//bool queue::isEmpty()
+//{
+//    return (size() == 0);
+//}
+//
+//// Utility function to check if the queue is full or not
+//bool queue::isFull()
+//{
+//    return (capacity = size() - 1);
+//}
+
 //Answers to analysis questions
 //1. All declarations and definitions were placed in main. Files should be broken up into .h and .cpp files for readability
 //2. Items in the queue were not actually being added. Values were printed, however the values were not being stored
--- testQueue.cpp
+++ testQueue.cpp
@@ -2,10 +2,20 @@
 #include "testQueue.h"
 
 /*************************************************************
-* Function: testSize()
-* Description: tests to see if the size function is working
-* Initial Error: test failed
-* Solution to Error: size was returning a number one higher than accurate
+* Test ID: testSize()
+Unit: queue::size ()
+Description: test to determine if queue::size () is working correctly
+Test steps:
+1. Construct an empty queue object
+2. add values to queue
+3. see if sizes matches number of values added 
+Test data: size = 3
+Precondition: queue object is empty
+Postcondition: queue object has 3 items
+Expected result: queue has three items
+Actual result: size did not match 3
+Status: failed
+Solution to Failure: size was returning a number one higher than accurate
 *************************************************************/
 void testSize(void)
 {
@@ -25,10 +35,20 @@ void testSize(void)
 }
 
 /*************************************************************
-* Function: testIsEmpty()
-* Description: tests to see if the isEmpty function is working
-* Initial Error: test failed
-* Solution to Error: function used size(), test passed when size() was fixed
+* Test ID: testIsEmpty()
+Unit: queue::isEmpty ()
+Description: test to determine if queue::isEmpty () is working correctly
+Test steps:
+1. Construct an empty queue object
+2. call isEmpty test
+3. if comes back true, test passed
+Test data: size = 3
+Precondition: queue object is empty
+Postcondition: queue object is empty
+Expected result: queue comes back as empty
+Actual result: queue did not come back as empty
+Status: failed
+Solution to Failure: function used size(), test passed when size() was fixed
 *************************************************************/
 void testIsEmpty(void)
 {
@@ -46,10 +66,21 @@ void testIsEmpty(void)
 }
 
 /*************************************************************
-* Function: testIsFull()
-* Description: tests to see if the IsFull function is working
-* Initial Error: test passed
-* Solution to Error: none needed
+* Test ID: testIsFull()
+Unit: queue::isFull ()
+Description: test to determine if queue::isFull () is working correctly
+Test steps:
+1. Construct an empty queue object
+2. Add values to queue to fill it
+3. call isFull test
+4. if comes back true, test passed
+Test data: size = 3
+Precondition: queue object is full
+Postcondition: queue object is full
+Expected result: queue comes back as full
+Actual result: queue came back as full
+Status: passed
+Solution to Failure: none needed
 *************************************************************/
 void testIsFull(void)
 {
@@ -70,10 +101,20 @@ void testIsFull(void)
 }
 
 /*************************************************************
-* Function: testDequeueFull()
-* Description: tests to see if the dequeue function is working on an full queue
-* Initial Error: test failed
-* Solution to Error: function used size(), test passed when size() was fixed
+* Test ID: testDequeueEmpty()
+Unit: queue::dequeue ()
+Description: test to determine if queue::dequeue () is working correctly on a empty queue
+Test steps:
+1. Construct an empty queue object
+2. call dequeue
+3. if size == 0, test passed
+Test data: size = 3
+Precondition: queue object is empty
+Postcondition: queue object is empty
+Expected result: size comes back as empty
+Actual result: size came back as zero
+Status: passed
+Solution to Failure: none needed
 *************************************************************/
 void testDequeueEmpty(void)
 {
@@ -91,10 +132,21 @@ void testDequeueEmpty(void)
 }
 
 /*************************************************************
-* Function: testDequeueFull()
-* Description: tests to see if the dequeue function is working on an full queue
-* Initial Error: test failed
-* Solution to Error: function used size(), test passed when size() was fixed
+* Test ID: testDequeueFull()
+Unit: queue::dequeue ()
+Description: test to determine if queue::dequeue () is working correctly on a full queue
+Test steps:
+1. Construct an empty queue object
+2. add 3 values to fill queue
+3. call dequeue
+4. if size == 2, test passed
+Test data: size = 3
+Precondition: queue object is full
+Postcondition: queue object is one less than full
+Expected result: size comes back as 2
+Actual result: size came back as not 2
+Status: failed
+Solution to Failure: function used size(), test passed when size() was fixed
 *************************************************************/
 void testDequeueFull(void)
 {
@@ -114,10 +166,20 @@ void testDequeueFull(void)
 }
 
 /*************************************************************
-* Function: testEnqueueEmpty()
-* Description: tests to see if the enqueue function is working on an empty queue
-* Initial Error: test failed
-* Solution to Error: function used size(), test passed when size() was fixed
+* Test ID: testEnqueueEmpty()
+Unit: queue::enqueue ()
+Description: test to determine if queue::enqueue () is working correctly on a empty queue
+Test steps:
+1. Construct an empty queue object
+2. call enqueue
+3. if size == 1, test passed
+Test data: size = 3
+Precondition: queue object is empty
+Postcondition: queue object has one object
+Expected result: size comes back as one
+Actual result: size came back as not one
+Status: failed
+Solution to Failure: function used size(), test passed when size() was fixed
 *************************************************************/
 void testEnqueueEmpty(void)
 {
@@ -134,10 +196,20 @@ void testEnqueueEmpty(void)
 }
 
 /*************************************************************
-* Function: testEnqueueNotEmpty()
-* Description: tests to see if the enqueue function is working on a not empty queue
-* Initial Error: test failed
-* Solution to Error: function used size(), test passed when size() was fixed
+* Test ID: testEnqueueNotEmpty()
+Unit: queue::enqueue ()
+Description: test to determine if queue::enqueue () is working correctly on a not empty
+Test steps:
+1. Construct an empty queue object
+2. add 2 values to queue
+3. if size == 2, test passed
+Test data: size = 3
+Precondition: queue object has two values
+Postcondition: queue object has two values
+Expected result: size comes back as 2
+Actual result: size came back as not 2
+Status: failed
+Solution to Failure: function used size(), test passed when size() was fixed
 *************************************************************/
 void testEnqueueNotEmpty(void)
 {
@@ -155,10 +227,19 @@ void testEnqueueNotEmpty(void)
 }
 
 /*************************************************************
-* Function: testPeekEmpty()
-* Description: tests to see if the peek function is working on an empty queue
-* Initial Error: test passed
-* Solution to Error: none needed
+* Test ID: testPeekEmpty()
+Unit: queue:: peek ()
+Description: test to determine if queue::peek () is working correctly on a empty queue
+Test steps:
+1. Construct an empty queue object
+2. call peek on queue
+Test data: size = 3
+Precondition: queue object is empty
+Postcondition: queue object peek returns nothing
+Expected result: peek came back with nothing
+Actual result: peek came back with nothing
+Status: passed
+Solution to Failure: none needed
 *************************************************************/
 void testPeekEmpty(void)
 {
@@ -175,10 +256,20 @@ void testPeekEmpty(void)
 }
 
 /*************************************************************
-* Function: testPeekFull()
-* Description: tests to see if the peek function is working on an full queue
-* Initial Error: test failed
-* Solution to Error: two issues needed to be fixed. Peek is supposed to return what is at the front of the queue,
+* Test ID: testPeekFull()
+Unit: queue:: peek ()
+Description: test to determine if queue::peek () is working correctly on a full queue
+Test steps:
+1. Construct an empty queue object
+2. add three values to queue
+3. call peek and see if value matches first value added
+Test data: size = 3
+Precondition: queue object is empty
+Postcondition: queue object has three values
+Expected result: peek came back with expected value
+Actual result: peek came back with queue index
+Status: failed
+Solution to Failure: two issues needed to be fixed. Peek is supposed to return what is at the front of the queue,
 * previous function was trying to return rear. Additionally, items were not actually being added to the queue, so their
 * values could not actually be returned using the old version of peek
 *************************************************************/
